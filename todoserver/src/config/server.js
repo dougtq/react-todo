48041e3fcda2fddeebd86f94e8bcc390e46a8c44
Douglas Alves <douglas.expalves@gmail.com>
2017-11-25 20:23:10 -0200

    added cors and exported server var

--- todoserver/src/config/server.js
+++ todoserver/src/config/server.js
@@ -1,10 +1,15 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./cors')
+
 let server = Express()
 
 server.set('port', 3000)
 server.use(bodyParser.urlencoded({ extended: true }))
+server.use(allowCors)
 
 server.listen(server.set('port'), () => {
   console.log('Servidor rodando na porta ', server.set('port'))
 })
+
+module.exports = server
