0712b57f1f0dcff6991b360636ce792bc5c73863
Douglas Alves <douglas.expalves@gmail.com>
2017-12-09 02:08:05 -0200

    Todo main component updated

--- todoapp/src/todo/Todo.jsx
+++ todoapp/src/todo/Todo.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
+import PageHeader from '../template/pageHeader'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
 export default class Todo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      description: '',
+      list: []
+    }
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleAdd () {
+    console.log('Add')
+  }
+
+  handleChange () {
+    console.log('Change')
+  }
+
   render () {
     return (
       <div className='container'>
-        <h1>Todo</h1>
+        <PageHeader title='Suas' subtitle='Tarefas' />
+        <TodoForm description={this.state.description} handleAdd={this.handleAdd} />
+        <TodoList />
       </div>
     )
   }
