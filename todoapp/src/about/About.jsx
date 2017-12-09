82b56846e8c1847cd5429f403a6ae81c119308db
Douglas Alves <douglas.expalves@gmail.com>
2017-12-09 02:07:44 -0200

    about_us example

--- todoapp/src/about/About.jsx
+++ todoapp/src/about/About.jsx
@@ -5,7 +5,9 @@ import PageHeader from '../template/pageHeader'
 export default props => (
   <div className='container'>
     <PageHeader title='Sobre' subtitle='Nós' />
-    <h3>Nossa História:</h3>
-    <p>Descreva a história da empresa</p>
+    <h3>Nossa História</h3>
+    <p>Descreva a história</p>
+    <h3>Missão e Valores</h3>
+    <p>Descreva a missão e os valores</p>
   </div>
 )
