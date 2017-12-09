715ead4d97d786fac087a1cbb0dc9aa5dfbc25cc
Douglas Alves <douglas.expalves@gmail.com>
2017-12-09 02:07:14 -0200

    update pageHeader

--- todoapp/src/template/pageHeader.jsx
+++ todoapp/src/template/pageHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => (
-    <header className='page-header'>
-        <h2>{props.title} <em>{props.subtitle}</em></h2>
-    </header>
+  <header className='page-header'>
+    <h2>{props.title} <em>{props.subtitle}</em></h2>
+  </header>
 )
