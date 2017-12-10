6c796f8f3dd672e64252a74b03e054c75029f201
Douglas Alves <douglas.expalves@gmail.com>
2017-12-10 20:56:54 -0200

    completed todolist functionalities and style customizations

--- todoapp/src/todo/TodoList.jsx
+++ todoapp/src/todo/TodoList.jsx
@@ -1,9 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class TodoList extends Component {
-  render () {
-    return (
-      <div> <h2>Lista</h2> </div>
+import IconButton from '../template/IconButton'
+
+export default props => {
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map((tarefa, i) =>
+      (<tr key={tarefa._id}>
+        <td className={tarefa.done ? 'markedAsDone' : ''}>{tarefa.description}</td>
+        <td>
+          <IconButton style='success' icon='check' hide={tarefa.done} onClick={() => props.handleMarkAsDone(tarefa)} />
+          <IconButton style='warning' icon='undo' hide={!tarefa.done} onClick={() => props.handleMarkAsPending(tarefa)} />
+          <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(tarefa)} />
+        </td>
+      </tr>)
     )
   }
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th> Descrição </th>
+          <th> Ações </th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
 }
