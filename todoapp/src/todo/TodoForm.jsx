9165a3eb358556352eee679fe417efd1dc775b71
Douglas Alves <douglas.expalves@gmail.com>
2017-12-10 20:57:39 -0200

    added search method for the search button

--- todoapp/src/todo/TodoForm.jsx
+++ todoapp/src/todo/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import Grid from '../template/Grid'
 import IconButton from '../template/IconButton'
 
@@ -6,12 +7,13 @@ export default class TodoForm extends Component {
   render () {
     return (
       <div role='form' className='TodoForm'>
-        <Grid cols={[12, 9, 10]}>
+        <Grid cols={[10, 8, 8]}>
           <input id='description' className='form-control'
-            placeholder='Crie uma tarefa' value={this.props.description} />
+            placeholder='Crie uma tarefa' onChange={this.props.handleChange} value={this.props.description} />
         </Grid>
         <Grid cols={[12, 3, 2]}>
           <IconButton style='primary' onClick={this.props.handleAdd} icon='plus' />
+          <IconButton style='info' onClick={this.props.handleSearch} icon='search' />
         </Grid>
       </div>
     )
