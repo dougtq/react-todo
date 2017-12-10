212fc40c19970da508e21ba56978622eabad7379
Douglas Alves <douglas.expalves@gmail.com>
2017-12-10 20:54:35 -0200

    update custom.css and imported to the proj

--- todoapp/src/main/App.jsx
+++ todoapp/src/main/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Menu from '../template/Menu'
 import Routes from './Routes'
 
+import '../template/custom.css'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
