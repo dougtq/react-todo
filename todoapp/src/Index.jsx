d9f5159a5409b002b08393e288359a705c8f1fec
Douglas Alves <douglas.expalves@gmail.com>
2018-02-08 02:48:03 -0200

    add redux_devtools, redux middlewares and reducers

--- todoapp/src/Index.jsx
+++ todoapp/src/Index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reduxPromise from 'redux-promise'
+import reduxMulti from 'redux-multi'
+import reduxThunk from 'redux-thunk'
 
 import App from './main/App'
+import reducers from './main/Reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && __REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(reduxMulti, reduxPromise, reduxThunk)(createStore)(reducers, devTools)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </ Provider>,
     document.getElementById('root')
 )
