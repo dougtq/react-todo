c058b995353f9184796f2dc995abbbdf4d056876
Douglas Alves <douglas.expalves@gmail.com>
2018-02-08 19:29:05 -0200

    add search changeStatus, remove and clear functions

--- todoapp/src/todo/TodoActions.js
+++ todoapp/src/todo/TodoActions.js
@@ -7,20 +7,39 @@ export const changeDescription = (ev) => ({
   payload: ev.target.value
 })
 
-export const search = (description = '') => {
-  let searchMethod = description ? `&description__regex=/${description}/` : ''
-  let request = axios.get(`${url}?sort=-createdAt${searchMethod}`)
-
-  return {
-    type: 'TODO_SEARCHED',
-    payload: request
+export const search = () => {
+  return (dispatch, getState) => {
+    const { description } = getState().todo
+    const searchMethod = description ? `&description__regex=/${description}/` : ''
+    axios.get(`${url}?sort=-createdAt${searchMethod}`)
+      .then((resp) => dispatch({ type: 'TODO_SEARCHED', payload: resp.data }))
   }
 }
 
 export const add = (description) => {
   return dispatch => {
     axios.post(url, { description })
-      .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+      .then(resp => dispatch(clear()))
       .then((resp) => dispatch(search()))
   }
+}
+
+export const changeStatus = (todo) => {
+  return dispatch => {
+    axios.put(`${url}/${todo._id}`, { ...todo, done: !todo.done })
+      .then(resp => dispatch({ type: 'TODO_STATUS_UPDATED', payload: resp.data }))
+      .then((resp) => dispatch(search()))
+  }
+}
+
+export const remove = (_id) => {
+  return dispatch => {
+    axios.delete(`${url}/${_id}`)
+      .then(resp => dispatch({ type: 'TODO_REMOVED', payload: resp.data }))
+      .then(res => dispatch(search()))
+  }
+}
+
+export const clear = () => {
+  return [{ type: 'TODO_CLEAR' }, search()]
 }
