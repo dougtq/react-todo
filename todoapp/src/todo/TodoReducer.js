eba2691af30deb35eb6a4a5209a6a2bb1f5e3b98
Douglas Alves <douglas.expalves@gmail.com>
2018-02-08 02:51:23 -0200

    add reducers switch

---
+++ todoapp/src/todo/TodoReducer.js
@@ -0,0 +1,24 @@
+const INITIAL_STATE = {
+  description: '',
+  list: []
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return { ...state, description: action.payload }
+      break
+
+    case 'TODO_SEARCHED':
+      return { ...state, list: action.payload.data }
+      break
+
+    case 'TODO_ADDED':
+      return { ...state, description: '' }
+      break
+      
+    default:
+      return state
+      break
+  }
+}
